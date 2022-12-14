b99d4021530a1e79d3b27b237efb57796352d5d5
taku <takuya_hiraoka@mail.toyota.co.jp>
2022-12-14 15:58:51 +0900

    feat(src/server.js):first schelton mission

---
+++ aj1-app/src/db/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+// const e = require("express");
+const express = require("express");
+// const db = require("./data");
+// const knex = require('./index');
+
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+app.use("/", express.static(__dirname + "../../public"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/", (req, res) => {
+    let message = "<h1>Hello world! From Server express.js!!!</h1>"
+    res.status(200).send(message);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Is your server running? Well, you better go catch it, then! http://localhost:${PORT}`);
+});
