7a06a4739efb273b9d545c681c0d54e21a6954e1
yuko827 <yuko.banno@toyota.global>
2022-12-30 18:03:36 +0900

    :bug: Fixed a bug when transitioning to the approver screen

--- aj1-app/src/component/ApproveBudge.jsx
+++ aj1-app/src/component/ApproveBudge.jsx
@@ -35,7 +35,6 @@ export default function ApproveBudge({setScreen, screen, admin}) {
                 </thead>
                 {applyList.map((record,key)=>{
                     return(
-                    <>
                         <tbody className="application" key={key}>
                             <tr>
                                 <th>{record.user_name}</th>
@@ -67,7 +66,6 @@ export default function ApproveBudge({setScreen, screen, admin}) {
                                 </th>
                             </tr>
                         </tbody>
-                    </>
                     )
                 })}
             </Table>
