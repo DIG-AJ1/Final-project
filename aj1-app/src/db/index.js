4e6ddc196936dd9200fcaf1168ddb6eeab597ee9
yamakudzu <mt.kuzu@gmail.com>
2023-01-10 10:51:11 +0900

    fix: index.js modify environment

--- aj1-app/src/db/index.js
+++ aj1-app/src/db/index.js
@@ -3,6 +3,7 @@ const knex = require("knex");
 const knexConfig = require("./knexfile");
 
 // const environment = process.env.DATABASE_URL ? "production" : "development";
-const environment = "production";
+// const environment = "production";
+const environment = "development";
 
 module.exports = knex(knexConfig[environment]);
