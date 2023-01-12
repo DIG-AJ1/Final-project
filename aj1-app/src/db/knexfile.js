58baac71b2988396db4ecc625b6bc3a3afa547c0
1579485-suganami <suganami,atsushi@mail.toyota.co.jp>
2023-01-12 15:11:31 +0900

    test99-error-after

--- aj1-app/src/db/knexfile.js
+++ aj1-app/src/db/knexfile.js
@@ -5,35 +5,35 @@
  */
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: {
-      user: process.env.POSTGRES_USER || "user",
-      database: process.env.POSTGRES_DB || "finalproject",
-      password: process.env.PASSWORD
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: { directory: "./data/seeds" },
-  },
-
   // development: {
   //   client: 'pg',
   //   connection: {
-  //     host: "aws-test2-infra-db-postgres.cmrbxlzkmnbb.ap-northeast-1.rds.amazonaws.com",
-  //     user: "root",
-  //     password: "test2password",
-  //     database: "finalproject",
-  //     charset: "utf8"
-  //   },
-  //   ssl: {
-  //     rejectUnauthorized: false
+  //     user: process.env.POSTGRES_USER || "user",
+  //     database: process.env.POSTGRES_DB || "finalproject",
+  //     password: process.env.PASSWORD
   //   },
   //   migrations: {
   //     directory: "./data/migrations",
   //   },
   //   seeds: { directory: "./data/seeds" },
-  // }
+  // },
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: "aws-test2-infra-db-postgres.cmrbxlzkmnbb.ap-northeast-1.rds.amazonaws.com",
+      user: "root",
+      password: "test2password",
+      database: "finalproject",
+      charset: "utf8"
+    },
+    ssl: {
+      rejectUnauthorized: false
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  }
 
 };
