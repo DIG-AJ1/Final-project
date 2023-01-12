4d6cf429b7483912ce7131d0a6869755e695ab75
Masao_Asano <masao_asano@mail.toyota.co.jp>
2023-01-12 14:45:09 +0900

    fix:requestbudge-page

--- aj1-app/src/db/index.js
+++ aj1-app/src/db/index.js
@@ -2,6 +2,10 @@
 const knex = require("knex");
 const knexConfig = require("./knexfile");
 
-const environment = process.env.DATABASE_URL ? "production" : "development";
+// const environment = process.env.DATABASE_URL ? "production" : "development";
+
+// const environment= "production"
+const environment= "development"
+
 
 module.exports = knex(knexConfig[environment]);
